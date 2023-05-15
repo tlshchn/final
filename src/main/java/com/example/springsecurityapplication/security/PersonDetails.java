2cb1368a735b245b40750df2f4f9e6cf15c41555
tlshchn <mtolshchin@yandex.ru>
2023-05-15 16:48:45 +0500

    first commit

---
+++ src/main/java/com/example/springsecurityapplication/security/PersonDetails.java
@@ -0,0 +1,61 @@
+package com.example.springsecurityapplication.security;
+
+import com.example.springsecurityapplication.models.Person;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    public PersonDetails(Person person) {
+        this.person = person;
+    }
+
+    public Person getPerson(){
+        return this.person;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(person.getRole()));
+    }
+
+
+
+    @Override
+    public String getPassword() {
+        return this.person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.person.getLogin();
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
